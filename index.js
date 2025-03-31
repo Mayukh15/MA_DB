6360b7146992fb0f0857f5c657c4fa2f5ad5339e
Mayukh15 <mayukhadhikari7@gmail.com>
2025-03-31 11:39:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKV,Mayukh adhikari')
+  res.send('Hello MCKV,here Mayukh')
   
 })
 
